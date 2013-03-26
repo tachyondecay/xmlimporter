07ed413e4c19a82302b37f4a18d473e28185e0c7
Ben Babcock <ben@tachyondecay.net>
2013-03-26 21:04:54 +0000

    Tweak xmlimporterhelpers to fix review processing.

--- lib/class.xmlimporterhelpers.php
+++ lib/class.xmlimporterhelpers.php
@@ -79,8 +79,6 @@
 				return "No review";
 			}
 			$str_replacement = array(
-				'<a href="http://creativecommons.org/licenses/by-nc/3.0/" rel="nofollow">' . "\n" . '  <img alt="Creative Commons BY-NC License" width="88" height="31" src="http://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a>'
-					=> '', // Remove the CC license
 				'class="escapedImg"' => 'class="inline"', // Change class on image tags
 				' rel="nofollow"' => '',
 				' target="_blank"' => '',
@@ -88,8 +86,9 @@
 
 			$preg_replacement = array(
 				"#<br /><strong>My reviews(.*?)</strong><br />(.*)<br /><br />#i" => '', // Remove series navigation, if present
-				"#<em>([A-Z]{1}\S+ [A-Za-z][^<]+)</em>#" => "<cite>$1</cite>", // Try to convert book titles to <cite> tags
-				"#<i>([A-Z]{1}\S+ [A-Za-z][^<]+)</i>#" => "<cite>$1</cite>",
+				"#<em>([A-Z0-9]{1}\S+?[A-Za-z][^<]+)</em>#" => "<cite>$1</cite>", // Try to convert book titles to <cite> tags
+				"#<i>([A-Z]{1}\S+?[A-Za-z][^<]+)</i>#" => "<cite>$1</cite>",
+				'#<a href="http://creativecommons.org(.*)</a>#isU' => '',
 			);
 
 			$text = str_replace(array_keys($str_replacement), array_values($str_replacement), $text);
